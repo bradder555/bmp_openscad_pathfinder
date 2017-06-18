1a2a4904f271c9d18a727b798ce8e8c1003cd1e2
Bradley Phillips <brad.phillips@exemail.com.au>
2017-06-18 12:44:28 +0800

    removes unnecessary prints, delete's imshow when locating the entry point to speed up the process

--- main.py
+++ main.py
@@ -3,16 +3,12 @@ from cv2 import *
 
 from tkinter import Tk
 from tkinter import filedialog
-import os
-import time
 
 root = Tk()
 root.withdraw()
 
 input_file = filedialog.askopenfilename()
-
 image = imread(input_file, 0)
-print(image[0, 0])
 
 im_width, im_height = image.shape
 
@@ -24,6 +20,8 @@ output_masked = output_im.copy()
 output_masked = bitwise_and(output_im, output_im, mask=image)
 namedWindow("Processing", WINDOW_NORMAL)
 start_location = (0, 0)
+imshow('Processing', output_masked)
+waitKey(1)
 
 # find the first black pixel
 for x in range(0, im_width):
@@ -33,10 +31,10 @@ for x in range(0, im_width):
             start_location = (x, y)
             break
         new_im[x, y] = (0, 0, 255)
-        imshow('Processing', new_im)
+        # imshow('Processing', new_im)
 
-        if waitKey(1) == ord('q'):
-            break
+        #if waitKey(1) == ord('q'):
+        #    break
     else:
         continue
     break
@@ -54,23 +52,23 @@ search_pattern = [(-1, -1),
 searching = True
 cursor_location = start_location
 points = []
+points.append(start_location)
 while searching:
     x, y = cursor_location
-    points.append(cursor_location)
+    #points.append(cursor_location)
     was_light = False
     last_tried = ()
     #look at adjacent bits
     for (adj_x, adj_y) in search_pattern:
         current_try = (x + adj_x, y + adj_y)
-        print(current_try)
         if image[current_try] == 255:
-            print("white")
             was_light = True
             last_try = current_try
 
         if image[x + adj_x, y + adj_y] == 0 and was_light:
-            print("black")
             cursor_location = current_try
+            points.append(current_try)
+            print(current_try)
             break
 
         if cursor_location == start_location and len(points) > 1:
